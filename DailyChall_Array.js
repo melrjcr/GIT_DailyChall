90834e4d2bd6a0a1ed9a0e91c605ab85604cddbd
melrjcr <57130610+melrjcr@users.noreply.github.com>
2019-11-05 21:34:06 +0200

    Add files via upload
    Daily Challenge- Array Challenge

---
+++ DailyChall_Array.js
@@ -0,0 +1,26 @@
+let array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+//1. Remove Banana
+let remove = array.shift();
+console.log(array)
+
+
+//2. sort 
+let sort = array.sort()
+console.log(array)
+
+//3. add to the end
+let push = array.push();
+console.log(array);
+
+//4.  remove banana
+let remove2 = array.shift();
+console.log(array)
+
+//5. reverse the array
+let reverse = array.reverse();
+console.log(array);
+
+// 6.remaining 
+var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+console.log(array2[1][1]);
